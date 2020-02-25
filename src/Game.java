a50a8bbe18bd039bb047ea4874897391b6031c83
Charles <carloseduarcma@gmail.com>
2020-02-25 09:01:12 -0800

    Add miniproject master

---
+++ src/Game.java
@@ -0,0 +1,129 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Game {
+
+    /*
+    *
+    *   VARIABLES
+    *
+    * */
+
+    private static String FILE_PATH = "cities.txt";
+    private static List<String> cities = new ArrayList<>();
+    private static List<Character> guessedCity = new ArrayList<Character>();
+    private static List<Character> underscoreCity = new ArrayList<Character>();
+    private static List<Character> usedLetters = new ArrayList<Character>();
+    private static int numOfGuesses = 0;
+
+    /*
+     *
+     *  CONSTRUCTOR
+     *
+     * */
+
+    public Game() {
+        this.guessedCity= new ArrayList<Character>();
+    }
+
+
+    /*
+    *
+    *   METHODS
+    *
+    * */
+
+    public void newGame(){
+        int times = 1;
+        boolean isWin = false;
+        createCities();
+        choseRandomCity();
+        underscoredCity();
+        System.out.println(guessedCity+"   size¨:"+guessedCity.size());
+        System.out.println(underscoreCity+"   size¨:"+underscoreCity.size());
+        Scanner scan = new Scanner(System.in);
+        char letter;
+        do{
+            String input = scan.nextLine();
+            letter = input.charAt(0);
+            boolean found = false;
+            if (validInput(letter)){
+                for (int x = 0; x < guessedCity.size();x++){
+                    if(underscoreCity.get(x).equals(letter)){
+                        usedLetters.add(letter);
+                        numOfGuesses++;
+                    }else if (guessedCity.get(x).equals(letter)){
+                        underscoreCity.set(x,letter);
+                        found= true;
+                    }
+                }
+                if (!found){
+                    usedLetters.add(letter);
+                    numOfGuesses++;
+                }
+                System.out.println(underscoreCity);
+                System.out.print("You have guessed ("+numOfGuesses+") wrong letters:");
+                for (char myLetter :usedLetters){
+                    System.out.print(myLetter);
+                }
+                System.out.println("");
+                System.out.println(!underscoreCity.contains("_"));
+                if (!underscoreCity.contains('_')){
+                    isWin = true;
+                    break;
+                }
+            }else{
+                System.out.println("Please try again, remember that you only can put 1 character and must be between A/a to Z/z");
+            }
+            times++;
+        }while(times<=10);
+        if (isWin){
+            System.out.println("You win!");
+        }else{
+            System.out.println("You lose!");
+        }
+    }
+
+    public static void createCities(){
+        File file = new File(FILE_PATH);
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String city = "";
+            while ((city = br.readLine()) != null) {
+                cities.add(city);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Error reading: " + FILE_PATH);
+        }
+    }
+    private void choseRandomCity() {
+        Random random = new Random();
+        String city = cities.get(random.nextInt(cities.size()));
+        for (char letter : city.toCharArray()) {
+            guessedCity.add(letter);
+        }
+    }
+
+    public static boolean validInput(Character letter){
+        if(letter >= 65 && letter <= 122){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public static void underscoredCity(){
+        for (int i = 0; i < guessedCity.size(); i++) {
+            if (guessedCity.get(i) == ' ') {
+                underscoreCity.add((" ").charAt(0));
+            } else {
+                underscoreCity.add(("_").charAt(0));
+            }
+        }
+    }
+}
